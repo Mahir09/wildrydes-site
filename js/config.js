837e06f22d4ef97b36b0407f21a05445421c781c
Mahir Shekh <72331249+Mahir09@users.noreply.github.com>
2025-01-11 10:05:16 -0500

    Update config.js with New User Pool Information

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_dOHrkypPT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4oc771a063hb6b399n8tokdd9r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_3FH9ITee3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '662iaoactn0mn20pnepg57ck6o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
